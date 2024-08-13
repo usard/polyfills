c1129a848a0be985eef49f069612ddb33e3089fa
usard <umasai.anvesh@gmail.com>
2024-08-13 09:59:17 +0530

    added checking from a particular index

--- includes/index.js
+++ includes/index.js
@@ -1,18 +1,26 @@
 let arr = [1,2,3,4,5,6]
-arr.includes()
-arr.includes(3)  // true
+// arr.includes()
+// arr.includes(3)  // true
 
 let obj = {id:1}
-arr.push(obj)
-Array.prototype.myincludes = function(targetElement){
-    // console.log('this :', targetElement )
-    for (let i=0;i<this.length; i++) {
-        if (this[i] == targetElement) {
+// arr.push(obj)
+Array.prototype.myincludes = function(targetElement, fromIndex=0){
+    if (this.length == 0 ) {
+        return false
+    }
+
+    let newIndex=0;
+    if (fromIndex < 0) {
+        newIndex = this.length + fromIndex
+    }
+
+    for ( let i = newIndex ;i<this.length; i++) {
+        if (this[i] === targetElement) {
             return true
         }
     }
     return false
 }
 
-console.log(arr.myincludes());
+console.log(arr.myincludes(3,-4));
 
