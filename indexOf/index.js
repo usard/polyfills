7444be25725190a141f8f447ae108d60f7e72ec0
usard <umasai.anvesh@gmail.com>
2024-08-25 15:28:58 +0530

    implementated to search from specific index, handled for negative index case

--- indexOf/index.js
+++ indexOf/index.js
@@ -1,11 +1,20 @@
 var arr = [5,3, ,7,2,9] // sparse array means array having indexes with empty values
 
-Array.prototype.myindexOf = function (val) {
+Array.prototype.myindexOf = function (val, fromIndex=0) {
     var array = this;
-    if (array.length <= 0 ) {
+    var index = fromIndex
+    if (index < 0){
+        if (Math.abs(index) > array.length) {
+            index = Math.abs(index)%array.length
+        }
+        else {
+            index = array.length+index
+        }
+    } 
+    if (array.length <= 0 || index >= array.length) {
         return -1
     }
-    for (var i = 0 ; i < array.length ; i++ ) {
+    for (var i = index ; i < array.length ; i++ ) {
         if (array[i] !== undefined && array[i] === val) {
             return i
         }
@@ -13,5 +22,11 @@ Array.prototype.myindexOf = function (val) {
     return -1
 }
 
-console.log(arr.myindexOf(7))
-console.log(arr.indexOf(7))
+console.log(arr.myindexOf(7,-4))
+console.log(arr.indexOf(7, -4))
+
+
+//note :
+// can search even with negative index greater than negative of array length 
+// cannot search for a index greater than or equal to array length
+//will return -1 if u want to search for undefined in sparse array
